5a982409da39b7b391075c341d12fc1d6f0f5740
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-10-05 17:38:28 -0400

    added parser_context

--- app/services/role/role_service.py
+++ app/services/role/role_service.py
@@ -1,15 +1,15 @@
 from google.protobuf.json_format import MessageToDict
 from mongoengine.queryset import NotUniqueError
 from ...protos import RoleServicer, RoleMultipleResponse, RoleResponse, RoleTableResponse, RoleEmpty, add_RoleServicer_to_server
-from ...utils import parser_all_object, parser_one_object, not_exist_code, exist_code, paginate
+from ...utils import parser_all_object, parser_one_object, not_exist_code, exist_code, paginate, parser_context
 from ...utils.validate_session import is_auth
 from ...models import Roles
 from ..bootstrap import grpc_server
 
 class RoleService(RoleServicer):
     def table(self, request, context):
-        metadata = dict(context.invocation_metadata())
-        is_auth(metadata['auth_token'], '00_role_table')
+        auth_token = parser_context(context, 'auth_token')
+        is_auth(auth_token, '00_role_table')
         roles = Roles.objects
         response = paginate(roles, request.page)
         response = RoleTableResponse(**response)
@@ -17,8 +17,8 @@ class RoleService(RoleServicer):
         return response
 
     def get_all(self, request, context):
-        metadata = dict(context.invocation_metadata())
-        is_auth(metadata['auth_token'], '00_role_get_all')
+        auth_token = parser_context(context, 'auth_token')
+        is_auth(auth_token, '00_role_get_all')
         roles = parser_all_object(Roles.objects.all())
         response = RoleMultipleResponse(role=roles)
 
@@ -26,8 +26,8 @@ class RoleService(RoleServicer):
 
     def get(self, request, context):
         try:
-            metadata = dict(context.invocation_metadata())
-            is_auth(metadata['auth_token'], '00_role_get')
+            auth_token = parser_context(context, 'auth_token')
+            is_auth(auth_token, '00_role_get')
             role = Roles.objects.get(id=request.id)
             role = parser_one_object(role)
             response = RoleResponse(role=role)
@@ -39,8 +39,8 @@ class RoleService(RoleServicer):
 
     def save(self, request, context):
         try:
-            metadata = dict(context.invocation_metadata())
-            is_auth(metadata['auth_token'], '00_role_save')
+            auth_token = parser_context(context, 'auth_token')
+            is_auth(auth_token, '00_role_save')
             role_object = MessageToDict(request)
             role = Roles(**role_object).save()
             role = parser_one_object(role)
@@ -53,8 +53,8 @@ class RoleService(RoleServicer):
 
     def update(self, request, context):
         try:
-            metadata = dict(context.invocation_metadata())
-            is_auth(metadata['auth_token'], '00_role_update')
+            auth_token = parser_context(context, 'auth_token')
+            is_auth(auth_token, '00_role_update')
             role_object = MessageToDict(request)
             role = Roles.objects(id=role_object['id'])
 
@@ -71,8 +71,8 @@ class RoleService(RoleServicer):
         
     def delete(self, request, context):
         try:
-            metadata = dict(context.invocation_metadata())
-            is_auth(metadata['auth_token'], '00_role_delete')
+            auth_token = parser_context(context, 'auth_token')
+            is_auth(auth_token, '00_role_delete')
             role = Roles.objects.get(id=request.id)
             role = role.delete()
             response = RoleEmpty()
